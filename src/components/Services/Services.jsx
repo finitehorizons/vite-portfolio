b855b5fd20ab0a03a125f377f6f4ae40978d619a
Christopher Goodwin <goodwin.chris2021@gmail.com>
2023-12-21 23:01:21 -0600

    Create Services.jsx
    feat: added base services component

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,15 @@
+import "./services.scss";
+
+const Services = () => {
+    return (
+        <div className='services'>
+            <div className='textContainer'>
+                <p></p>
+            </div>
+            <div className='titleContainer'></div>
+            <div className='listContainer'></div>
+        </div>
+    );
+};
+
+export default Services;
