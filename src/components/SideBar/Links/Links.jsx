34b9d791fb14002119a9a7d645a7e900e4a9a4c5
Christopher Goodwin <goodwin.chris2021@gmail.com>
2023-12-22 22:49:15 -0600

    Update Links.jsx
    refactor: changed name of section on sidebar menu

--- src/components/SideBar/Links/Links.jsx
+++ src/components/SideBar/Links/Links.jsx
@@ -24,7 +24,7 @@ const itemVariants = {
 };
 
 const Links = () => {
-    const items = ["Home", "Services", "Portfolio", "Contact", "About"];
+    const items = ["Home", "Skills", "Portfolio", "Contact", "About"];
 
     return (
         <motion.div className='links' variants={variants}>
