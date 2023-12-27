92c5ce43faa13f8046b8145fb1cc9cdcd4a33ac0
Christopher Goodwin <goodwin.chris2021@gmail.com>
2023-12-26 23:08:48 -0600

    Update Services.jsx
    refactor: added classes to images for easier responsive styling, updated capitalization
    
    TODO: reinitiate isInView animation(disabled for responsive styling)

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -26,166 +26,174 @@ const Services = () => {
     const isInView = useInView(ref, { margin: "-200px" });
 
     return (
-        <motion.div
-            className='services'
-            variants={variants}
-            initial='initial'
-            // whileInView='animate'
-            ref={ref}
-            animate={isInView && "animate"}>
-            <motion.div className='textContainer'>
-                <p>
-                    I build things for the web.
-                    <br />
-                    modern. simple. beautiful.
-                </p>
-                <hr />
-            </motion.div>
-            <motion.div className='titleContainer' variants={variants}>
-                <div className='title'>
-                    <h1>
-                        <motion.b whileHover={{color: "orangered"}}>Limitless</motion.b> Possibilities
-                    </h1>
-                    <h4>Technologies, Frameworks and Tools</h4>
-                </div>
-            </motion.div>
-            <motion.div className='listContainer' variants={variants}>
-                <motion.div
-                    whileHover={{
-                        background: "#060E3C",
-                        filter: "brightness(1.9)",
-                    }}
-                    className='box'>
-                    <img src='/source.png' alt='source' />
-                    <h2 className='tech'>FRONT END</h2>
-
-                    <div className='wrapper'>
-                        <div className='section'>
-                            <ul className='languages'>
-                                <li>
-                                    <img src='/html.png' alt='' />
-                                    <p>HTML</p>
-                                </li>
-
-                                <li>
-                                    <img src='/js.png' alt='' />
-                                    <p>Javascript</p>
-                                </li>
-                                <li>
-                                    <img src='/react.png' alt='' />
-                                    <p>React</p>
-                                </li>
-                                <li>
-                                    <img src='/next.png' alt='' />
-                                    <p>NextJS</p>
-                                </li>
-                            </ul>
-                            <ul className='languages'>
-                                <li>
-                                    <img src='/css.png' alt='' />
-                                    <p>CSS</p>
-                                </li>
-                                <li>
-                                    <img src='sass.png' alt='' />
-                                    <p>Sass</p>
-                                </li>
-                                <li>
-                                    <img src='tailwind.png' alt='' />
-                                    <p>Tailwind</p>
-                                </li>
-                            </ul>
-                        </div>
+        <div>
+            <motion.div
+                className='services'
+                variants={variants}
+                initial='initial'
+                // whileInView='animate'
+                ref={ref}
+                animate={"animate"}>
+                <motion.div className='textContainer'>
+                    <p>
+                        I build things for the web.
+                        <br />
+                        modern. simple. beautiful.
+                    </p>
+                    <hr />
+                </motion.div>
+                <motion.div className='titleContainer' variants={variants}>
+                    <div className='title'>
+                        <h1>
+                            <motion.b whileHover={{ color: "orangered" }}>
+                                Limitless
+                            </motion.b>{" "}
+                            Possibilities
+                        </h1>
+                        <h4>Technologies, Frameworks and Tools</h4>
                     </div>
                 </motion.div>
-                <motion.div
-                    whileHover={{
-                        background: "#060E3C",
-                        filter: "brightness(1.9)",
-                    }}
-                    q
-                    className='box'
-                    variants={variants}>
-                    <img src='/db96.png' alt='' />
-                    <h2 className='tech'> BACKEND </h2>
-                    <div className='wrapper'>
-                        <div className='section'>
-                            <ul className='languages'>
-                                <li>
-                                    <img src='/python.png' alt='' />
-                                    <p>Python</p>
-                                </li>
+                <motion.div className='listContainer' variants={variants}>
+                    <motion.div
+                        whileHover={{
+                            background: "#060E3C",
+                            filter: "brightness(1.9)",
+                        }}
+                        className='box'>
+                        <img className="desc" src='/source.png' alt='source' />
+                        <h2 className='tech'>FRONT END</h2>
 
-                                <li>
-                                    <img src='/node.png' alt='' />
-                                    <p>NodeJS</p>
-                                </li>
-                                <li>
-                                    <img src='/mongo.png' alt='' />
-                                    <p>MongoDB</p>
-                                </li>
-                                <li>
-                                    <img src='/mysql.png' alt='' />
-                                    <p>MySQL</p>
-                                </li>
-                            </ul>
-                            <ul className='languages'>
-                                <li>
-                                    <img src='/icons8-django-64.png' alt='' />
-                                    <p>Django</p>
-                                </li>
-                            </ul>
+                        <div className='wrapper'>
+                            <div className='section'>
+                                <ul className='languages'>
+                                    <li>
+                                        <img src='/html.png' alt='' />
+                                        <p>HTML</p>
+                                    </li>
+
+                                    <li>
+                                        <img src='/js.png' alt='' />
+                                        <p>Javascript</p>
+                                    </li>
+                                    <li>
+                                        <img src='/react.png' alt='' />
+                                        <p>React</p>
+                                    </li>
+                                    <li>
+                                        <img src='/next.png' alt='' />
+                                        <p>NextJS</p>
+                                    </li>
+                                </ul>
+                                <ul className='languages'>
+                                    <li>
+                                        <img src='/css.png' alt='' />
+                                        <p>CSS</p>
+                                    </li>
+                                    <li>
+                                        <img src='sass.png' alt='' />
+                                        <p>Sass</p>
+                                    </li>
+                                    <li>
+                                        <img src='tailwind.png' alt='' />
+                                        <p>Tailwind</p>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                </motion.div>
-                <motion.div
-                    whileHover={{
-                        background: "#060E3C",
-                        filter: "brightness(1.9)",
-                    }}
-                    q
-                    className='box'
-                    variants={variants}>
-                    <img src='/tool96.png' alt='' />
-                    <h2 className='tech'> Tools </h2>
-                    <div className='wrapper'>
-                        <div className='section'>
-                            <ul className='languages'>
-                                <li>
-                                    <img src='/vscode.png' alt='' />
-                                    <p>VS Code</p>
-                                </li>
-                                <li>
-                                    <img src='/pycharm.png' alt='' />
-                                    <p>Pycharm</p>
-                                </li>
-                                <li>
-                                    <img src='/jetbrains.png' alt='' />
-                                    <p>Webstorm</p>
-                                </li>
-                                <li>
-                                    <img src='/aws.png' alt='' />
-                                    <p>AWS</p>
-                                </li>
-                            </ul>
-                            <ul className='languages'>
-                                <li>
-                                    <img src='/github2.png' alt='' />
-                                    <p>Github</p>
-                                </li>
-                                <li>
-                                    <img src='/figma.png' alt='' />
-                                    <p>Figma</p>
-                                </li>
-                                <li>
-                                    <img src='source.png' alt='' />
-                                    <p>Terminal</p>
-                                </li>
-                            </ul>
+                    </motion.div>
+                    <motion.div
+                        whileHover={{
+                            background: "#060E3C",
+                            filter: "brightness(1.9)",
+                        }}
+                        q
+                        className='box'
+                        variants={variants}>
+                        <img className="desc" src='/db96.png' alt='' />
+                        <h2 className='tech'> BACKEND </h2>
+                        <div className='wrapper'>
+                            <div className='section'>
+                                <ul className='languages'>
+                                    <li>
+                                        <img src='/python.png' alt='' />
+                                        <p>Python</p>
+                                    </li>
+
+                                    <li>
+                                        <img src='/node.png' alt='' />
+                                        <p>NodeJS</p>
+                                    </li>
+                                    <li>
+                                        <img src='/mongo.png' alt='' />
+                                        <p>MongoDB</p>
+                                    </li>
+                                    <li>
+                                        <img src='/mysql.png' alt='' />
+                                        <p>MySQL</p>
+                                    </li>
+                                </ul>
+                                <ul className='languages'>
+                                    <li>
+                                        <img
+                                            src='/icons8-django-64.png'
+                                            alt=''
+                                        />
+                                        <p>Django</p>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
+                    </motion.div>
+                    <motion.div
+                        whileHover={{
+                            background: "#060E3C",
+                            filter: "brightness(1.9)",
+                        }}
+                        q
+                        className='box'
+                        variants={variants}>
+                        <img className="desc" src='/tool96.png' alt='' />
+                        <h2 className='tech'> TOOLS </h2>
+                        <div className='wrapper'>
+                            <div className='section'>
+                                <ul className='languages'>
+                                    <li>
+                                        <img src='/vscode.png' alt='' />
+                                        <p>VS Code</p>
+                                    </li>
+                                    <li>
+                                        <img src='/pycharm.png' alt='' />
+                                        <p>Pycharm</p>
+                                    </li>
+                                    <li>
+                                        <img src='/jetbrains.png' alt='' />
+                                        <p>Webstorm</p>
+                                    </li>
+                                    <li>
+                                        <img src='/aws.png' alt='' />
+                                        <p>AWS</p>
+                                    </li>
+                                </ul>
+                                <ul className='languages'>
+                                    <li>
+                                        <img src='/github2.png' alt='' />
+                                        <p>Github</p>
+                                    </li>
+                                    <li>
+                                        <img src='/figma.png' alt='' />
+                                        <p>Figma</p>
+                                    </li>
+                                    <li>
+                                        <img src='source.png' alt='' />
+                                        <p>Terminal</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </motion.div>
                 </motion.div>
             </motion.div>
-        </motion.div>
+        </div>
     );
 };
 
