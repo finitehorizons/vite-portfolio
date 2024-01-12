d32f0dc42cdb5637ed2bdcb927892676e53acf67
Christopher Goodwin <goodwin.chris2021@gmail.com>
2024-01-11 18:07:33 -0600

    Update Navbar.jsx
    chore: updated navbar links to real end points

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,27 +2,27 @@ import Sidebar from "../SideBar/Sidebar";
 import "./navbar.scss";
 import { motion } from "framer-motion";
 
+import Reveal from "../Reveal/Reveal";
+
 const Navbar = () => {
     return (
         <div className='navbar'>
             {/* Sidebar */}
             <Sidebar />
             <div className='wrapper'>
-                <motion.span
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.5 }}>
-                    Christopher.dev
-                </motion.span>
+                    <motion.span
+                        initial={{ opacity: 0, scale: 0.5 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{ duration: 0.5 }}
+                        whileHover={{scale: 1.3, color: "rgb(255, 69, 0)"}}>
+                        Christopher.dev
+                    </motion.span>
                 <div className='social'>
-                    <a href='/'>
-                        <img src='/facebook.png' alt='' />
+                    <a href='https://www.github.com/finitehorizons'>
+                        <motion.img src='/github2.png' alt='' whileHover={{scale: 1.9}} transition={{type: "spring"}} />
                     </a>
-                    <a href='/'>
-                        <img src='/github2.png' alt='' />
-                    </a>
-                    <a href='/'>
-                        <img src='/linkedin.png' alt='' />
+                    <a href='https://www.linkedin.com/in/finitehorizons/'>
+                        <motion.img src='/linkedin.png' alt='' whileHover={{scale: 1.9}} transition={{type: "spring"}} />
                     </a>
                 </div>
             </div>
